77b4c26f1ae1c28fd9838d3b3894aa904c114163
chintan1808 <109083285+chintan1808@users.noreply.github.com>
2024-01-16 02:49:53 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+function App() {
+  const [ color, setcolor] = useState("white")
+
+  return (
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+
+        <button
+
+        onClick={() => setcolor("red")}
+          class="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+
+          <button
+          onClick={() => setcolor("green")}
+          class="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "Green"}}
+          >Green</button>
+
+
+          <button
+          onClick={() => setcolor("blue")}
+          class="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "Blue"}}
+          >Blue</button>
+
+          <button
+          onClick={() => setcolor("black")}
+          class="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "Black"}}
+          >Black</button>
+
+          <button
+          onClick={() => setcolor("purple")}
+          class="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "purple"}}
+          >Purple</button>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
